a81acd7280f6815d62deb21c920721c63fa7dbb4
Colin Wilson <github@colinwilson.dev>
2022-03-11 21:50:52 -0500

    Hook up to 5eSRD

--- cogs/general.py
+++ cogs/general.py
@@ -6,6 +6,10 @@ from nextcord.ext import commands
 from nextcord.ui import Button, View
 import subprocess
 import re
+from pathlib import Path
+import json
+import requests
+
 CLEANR = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
 
 if "DnDBot" not in str(os.getcwd()):
@@ -39,31 +43,79 @@ class general(commands.Cog, name="general"):
         cleantext = re.sub(CLEANR, '', text)
         return cleantext
 
+    def generate_embed(self, res, index, max_index):
+        embed = nextcord.Embed(
+            title=res["results"][index]["name"],
+            color=config["success"]
+        )
+        for key in res["results"][index]:
+            if key in ["name", "document_slug", "document_title", "route", "slug", "highlighted"]:
+                continue
+            value = res["results"][index][key]
+            if len(value) == 0:
+                continue
+            if len(value) > 1000:
+                value = value[:1000] + "..."
+            embed.add_field(
+                name=key,
+                value=value,
+                inline=False
+            )
+        embed.set_footer(
+            text=f"{index+1}/{max_index}"
+        )
+        return embed
+
     @commands.command(name="search")
     async def search(self, context, *query):
         """
-        Check if the bot is alive.
+        Search the 5e SRD
         """
-        try:
-            terms = " ".join(query)
-            res = self.remove_tags(subprocess.check_output(f"lookup5e {terms}", universal_newlines=True)[:1000])
-        except:
-            res = "Not found"
+        terms = " ".join(query)
+        res = json.loads(requests.get(f"https://api.open5e.com/search/?text={terms}").text)
+        count = res["count"]
+        self.currIndex = 0
 
-        embed = nextcord.Embed(
-            color=config["success"]
-        )
-        embed.add_field(
-            name="Result",
-            value=res,
-            inline=True
-        )
-        embed.set_footer(
-            text=f"Searched by: {context.message.author}"
-        )
+        for index, r in enumerate(res["results"]):
+            if r["name"].lower() == terms.lower():
+                self.currIndex = index
+                break
 
-        await context.send(embed=embed)
-    
+        embed = self.generate_embed(res, self.currIndex, count)
+        previous_button = Button(label="<", style=nextcord.ButtonStyle.red)
+
+        async def previous_callback(interaction):
+            # edit the embed to show the previous result
+            currEmbed = interaction.message.embeds[0].to_dict()
+            index = int(currEmbed["footer"]["text"].split("/")[0]) - 1
+            if (index + 1) < 0:
+                index = count - 1
+            else:
+                index = index - 1
+            newembed = self.generate_embed(res, index, count)
+            await interaction.message.edit(embed=newembed)
+        
+        next_button = Button(label=">", style=nextcord.ButtonStyle.red)
+
+        async def next_callback(interaction):
+            # edit the embed to show the previous result
+            currEmbed = interaction.message.embeds[0].to_dict()
+            index = int(currEmbed["footer"]["text"].split("/")[0]) - 1
+            if (index + 1) >= count:
+                index = 0
+            else:
+                index = index + 1
+            newembed = self.generate_embed(res, index, count)
+            await interaction.message.edit(embed=newembed)
+        
+        previous_button.callback = previous_callback
+        next_button.callback = next_callback
+
+        view = View(timeout=1000)
+        view.add_item(previous_button)
+        view.add_item(next_button)
+
+        await context.send(embed=embed, view=view)
 
 
 
